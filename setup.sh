9f2a5766f618095d4390c387135c004d2a140ecd
otherchris <chriscaragianis@gmail.com>
2023-07-02 12:02:28 -0400

    install neovim

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# install and configure lunarvim
+#
+# install neovim
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-macos.tar.gz
+xattr -c ./nvim-macos.tar.gz
+tar xzvf nvim-macos.tar.gz
+./nvim-macos/bin/nvim
+
+
+
+
+
