30f01c34005d539e238a0b53d37b37762565466d
otherchris <chriscaragianis@gmail.com>
2023-07-02 17:21:34 -0400

    install lunarvim

--- devenv.nix
+++ devenv.nix
@@ -8,6 +8,11 @@
   packages = [ 
     pkgs.git 
     pkgs.wget
+    pkgs.gnumake
+    pkgs.python311Packages.pip
+    pkgs.python311
+    pkgs.nodejs_20
+    pkgs.cargo
   ];
 
   # https://devenv.sh/scripts/
@@ -18,6 +23,14 @@
     mkdir -p ~/.local/bin/; cp -r nvim-macos/bin/* $_
     mkdir -p ~/.local/share/; cp -r nvim-macos/share/* $_
     mkdir -p ~/.local/lib/; cp -r nvim-macos/lib/* $_
+
+    mkdir ~/.npm-global
+    npm config set prefix '~/.npm-global'
+    echo "export PATH=~/.npm-global/bin:$PATH" > ~/.profile 
+    source ~/.profile 
+
+    LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+
   '';
  
   enterShell = ''
