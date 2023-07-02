684c91916a7a0d2f86c833b0acbc728a4f6ae28f
otherchris <chriscaragianis@gmail.com>
2023-07-02 13:21:06 -0400

    move nvim install

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 # install nix
 
-sh <(curl -L https://nixos.org/nix/install)
-nix-env -iA cachix -f https://cachix.org/api/v1/install
-cachix use devenv
+# sh <(curl -L https://nixos.org/nix/install)
+# nix-env -iA cachix -f https://cachix.org/api/v1/install
+# cachix use devenv
 nix-env -if https://github.com/cachix/devenv/tarball/latest
 
