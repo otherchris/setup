b5e8b8b2c2556b81825dc80468b1cf5ff537cf2f
otherchris <chriscaragianis@gmail.com>
2023-07-05 09:06:09 -0400

    moving away from nix

--- devenv.nix
+++ devenv.nix
@@ -17,19 +17,19 @@
 
   # https://devenv.sh/scripts/
   scripts.lvim-install.exec = ''
-    wget https://github.com/neovim/neovim/releases/download/stable/nvim-macos.tar.gz   
-    xattr -c ./nvim-macos.tar.gz
-    tar xzvf nvim-macos.tar.gz
-    mkdir -p ~/.local/bin/; cp -r nvim-macos/bin/* $_
-    mkdir -p ~/.local/share/; cp -r nvim-macos/share/* $_
-    mkdir -p ~/.local/lib/; cp -r nvim-macos/lib/* $_
+    # wget https://github.com/neovim/neovim/releases/download/stable/nvim-macos.tar.gz   
+    # xattr -c ./nvim-macos.tar.gz
+    # tar xzvf nvim-macos.tar.gz
+    # mkdir -p ~/.local/bin/; cp -r nvim-macos/bin/* $_
+    # mkdir -p ~/.local/share/; cp -r nvim-macos/share/* $_
+    # mkdir -p ~/.local/lib/; cp -r nvim-macos/lib/* $_
 
-    mkdir ~/.npm-global
-    npm config set prefix '~/.npm-global'
-    echo "export PATH=~/.npm-global/bin:$PATH" > ~/.profile 
-    source ~/.profile 
+    # mkdir ~/.npm-global
+    # npm config set prefix '~/.npm-global'
+    # echo "export PATH=~/.npm-global/bin:$PATH" > ~/.profile 
+    # source ~/.profile 
 
-    LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+    # LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
 
   '';
  
--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 
-# install nix
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-macos.tar.gz   
+xattr -c ./nvim-macos.tar.gz
+tar xzvf nvim-macos.tar.gz
+mkdir -p ~/.local/bin/; cp -r nvim-macos/bin/* $_
+mkdir -p ~/.local/share/; cp -r nvim-macos/share/* $_
+mkdir -p ~/.local/lib/; cp -r nvim-macos/lib/* $_
 
-# sh <(curl -L https://nixos.org/nix/install)
-# nix-env -iA cachix -f https://cachix.org/api/v1/install
-# cachix use devenv
-nix-env -if https://github.com/cachix/devenv/tarball/latest
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+echo "export PATH=~/.npm-global/bin:$PATH" > ~/.profile 
+source ~/.profile 
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+
 
